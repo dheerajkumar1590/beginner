28e5e99195b7c415a9f00fffa3c4d149f0e191a0
dheerajkumar1590 <104754256+dheerajkumar1590@users.noreply.github.com>
2022-05-05 20:03:04 +0530

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=10
+
+
+# In[2]:
+
+
+b=32
+
+
+# In[3]:
+
+
+a+b
+
+
+# In[4]:
+
+
+a="Dheeraj"
+
+
+# In[5]:
+
+
+input(b)
+
+
+# In[6]:
+
+
+a+b
+
+
+# In[7]:
+
+
+b
+
+
+# In[8]:
+
+
+b=input()
+
+
+# In[9]:
+
+
+a+b
+
+
+# c=1+3j
+
+# In[10]:
+
+
+type(c)
+
+
+# In[11]:
+
+
+c=2+3j
+
+
+# In[12]:
+
+
+type(c)
+
+
+# In[13]:
+
+
+a,b,c,d,e=12,12.3,'my first practice',1+2j
+
+
+# In[14]:
+
+
+a,b,c,d,e=12,12.3,'my first practice',1+2j,True
+
+
+# In[15]:
+
+
+type(e)
+
+
+# In[16]:
+
+
+type(c)
+
+
+# In[17]:
+
+
+type(a)
+
+
+# In[18]:
+
+
+type(b)
+
+
+# In[19]:
+
+
+type(d)
+
+
+# In[ ]:
+
+
+
+
---
+++ Untitled2.py
@@ -0,0 +1,540 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a='Dheer'
+
+
+# In[2]:
+
+
+type(a
+)
+
+
+# In[3]:
+
+
+type(D)
+
+
+# In[4]:
+
+
+a[0]
+
+
+# In[5]:
+
+
+a[1]
+
+
+# In[6]:
+
+
+a[2]
+
+
+# In[7]:
+
+
+a[3]
+
+
+# In[8]:
+
+
+a[4]
+
+
+# In[9]:
+
+
+a[-1]
+
+
+# In[10]:
+
+
+a[-5]
+
+
+# In[11]:
+
+
+b='my name is dheeraj'
+
+
+# In[12]:
+
+
+b[0
+]
+
+
+# In[13]:
+
+
+b[-1]
+
+
+# In[14]:
+
+
+b[100]
+
+
+# In[15]:
+
+
+b[0:10]
+
+
+# In[16]:
+
+
+c='college'
+
+
+# In[17]:
+
+
+c[0:3]
+
+
+# In[18]:
+
+
+c[0:300]
+
+
+# In[19]:
+
+
+b[-1]
+
+
+# In[20]:
+
+
+b[-100]
+
+
+# In[21]:
+
+
+b[-1:-4]
+
+
+# In[22]:
+
+
+d='kumar'
+
+
+# In[23]:
+
+
+d[0:4]
+
+
+# In[24]:
+
+
+d[0:300:1]
+
+
+# In[26]:
+
+
+d[0:300:3]
+
+
+# In[27]:
+
+
+d[0:100:-1]
+
+
+# In[29]:
+
+
+d[-1:-4:-1]
+
+
+# In[30]:
+
+
+d[0:-10:-1]
+
+
+# In[31]:
+
+
+d[::]
+
+
+# In[32]:
+
+
+d[:8]
+
+
+# In[34]:
+
+
+d[-2:-1]
+
+
+# In[35]:
+
+
+d[::-1]
+
+
+# In[36]:
+
+
+d[-1::-1]
+
+
+# In[37]:
+
+
+d[-1::-1]
+
+
+# In[38]:
+
+
+d[-1:]
+
+
+# In[41]:
+
+
+a="this is my second practice"
+
+
+# In[42]:
+
+
+a[::-1]
+
+
+# In[43]:
+
+
+a[-5:5]
+
+
+# In[44]:
+
+
+a[-5:5:-1]
+
+
+# In[45]:
+
+
+dheeraj*3
+
+
+# In[46]:
+
+
+'Dheeraj'*3
+
+
+# In[47]:
+
+
+'Dheeraj'+'Kumar'
+
+
+# In[48]:
+
+
+a
+
+
+# In[49]:
+
+
+len(a)
+
+
+# In[50]:
+
+
+a.find(a)
+
+
+# In[51]:
+
+
+a.find('a')
+
+
+# In[52]:
+
+
+a.find('a')
+
+
+# In[53]:
+
+
+a
+
+
+# In[54]:
+
+
+a.find('i')
+
+
+# In[55]:
+
+
+a.find('am')
+
+
+# In[56]:
+
+
+a.find('my')
+
+
+# In[57]:
+
+
+a.count('')
+
+
+# In[58]:
+
+
+a.count('i')
+
+
+# In[59]:
+
+
+a
+
+
+# In[60]:
+
+
+a.split()
+
+
+# In[61]:
+
+
+l=a.split()
+
+
+# In[62]:
+
+
+a[]1
+
+
+# In[63]:
+
+
+l[1]
+
+
+# In[64]:
+
+
+l[-1]
+
+
+# In[65]:
+
+
+l[0:10]
+
+
+# In[66]:
+
+
+l[0::-1]
+
+
+# In[67]:
+
+
+l[-1::1]
+
+
+# In[68]:
+
+
+l[-1::-1]
+
+
+# In[69]:
+
+
+a.split('i')
+
+
+# In[70]:
+
+
+a.upper()
+
+
+# In[71]:
+
+
+d='DhEeR'
+
+
+# In[72]:
+
+
+d.swapcase()
+
+
+# In[73]:
+
+
+d.title()
+
+
+# In[74]:
+
+
+d.capitalize()
+
+
+# In[75]:
+
+
+b='dheeraj'
+c='kumar'
+
+
+# In[76]:
+
+
+b.join(c)
+
+
+# In[78]:
+
+
+' '.join('dheeraj')
+
+
+# In[79]:
+
+
+reversed('dheeraj')
+
+
+# In[80]:
+
+
+for i in reversed('dheeraj')
+print(i)
+
+
+# In[81]:
+
+
+for i in reversed('dheeraj'):
+print(i)
+
+
+# In[82]:
+
+
+for i in reversed('dheeraj'):
+     print(i)
+
+
+# In[83]:
+
+
+a='dheeraj'
+a[::-1]
+
+
+# In[87]:
+
+
+a=" dheeraj "
+a.lstrip()
+
+
+# In[88]:
+
+
+a="dheeraj"
+
+
+# In[89]:
+
+
+a.replace('e','b')
+
+
+# In[92]:
+
+
+'dheeraj\tkumar'.expandtabs()
+
+
+# In[93]:
+
+
+a='dheeraj'
+
+
+# In[94]:
+
+
+a.center(40,'r')
+
+
+# In[95]:
+
+
+a="    i am enjoying practicing python "
+a.strip("practicing")
+
+
+# In[96]:
+
+
+a.strip()
+
+
+# In[97]:
+
+
+a.split()
+
+
+# In[99]:
+
+
+b=a.split()
+b.join()
+
+
+# In[ ]:
+
+
+
+
