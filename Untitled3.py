45a1c8e34ca5998e09e5b09aa240f63231a1e585
dheerajkumar1590 <104754256+dheerajkumar1590@users.noreply.github.com>
2022-05-06 15:18:39 +0530

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,391 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a='dheeraj'
+
+
+# In[2]:
+
+
+a.isupper()
+
+
+# In[3]:
+
+
+a='DHEERAJ'
+
+
+# In[4]:
+
+
+a.isupper()
+
+
+# In[5]:
+
+
+a.islower()
+
+
+# In[6]:
+
+
+a.isspace()
+
+
+# In[7]:
+
+
+a='    dheeraj'
+
+
+# In[8]:
+
+
+a.isspace()
+
+
+# In[9]:
+
+
+a=' d h e e r a j'
+
+
+# In[10]:
+
+
+a.isspace()
+
+
+# In[11]:
+
+
+a='         '
+
+
+# In[12]:
+
+
+a.isspace()
+
+
+# In[16]:
+
+
+a='12'
+
+
+# In[18]:
+
+
+a.isdigit()
+
+
+# In[19]:
+
+
+a.endswith('j')
+
+
+# In[20]:
+
+
+a='dheeraj'
+
+
+# In[21]:
+
+
+a.endswith('j')
+
+
+# In[22]:
+
+
+a.istitle()
+
+
+# In[24]:
+
+
+b=['dheeraj','kumar',123,True,32.9]
+
+
+# In[25]:
+
+
+type(b)
+
+
+# In[26]:
+
+
+b[-1]
+
+
+# In[27]:
+
+
+b[0]
+
+
+# In[28]:
+
+
+b[3]
+
+
+# In[29]:
+
+
+b[0:4]
+
+
+# In[30]:
+
+
+b[::-1]
+
+
+# In[31]:
+
+
+b[-1:6]
+
+
+# In[33]:
+
+
+type (b[0])
+
+
+# In[34]:
+
+
+type(b)
+
+
+# In[36]:
+
+
+type(b[0][2])
+
+
+# In[37]:
+
+
+b[0][2]
+
+
+# In[38]:
+
+
+a1=['dheeraj','kumar',12]
+a2=['xyz','pq',345.7]
+a1+a2
+
+
+# In[39]:
+
+
+a1=['dheeraj','kumar',12]
+a1+'good'
+
+
+# In[41]:
+
+
+a1=['dheeraj','kumar',12]
+a1+['good']
+
+
+
+# In[42]:
+
+
+a1*4
+
+
+# In[43]:
+
+
+
+
+
+# In[44]:
+
+
+a1
+
+
+# In[45]:
+
+
+a.replace('sudh')
+
+
+# In[46]:
+
+
+a[0]
+
+
+# In[47]:
+
+
+a1[0]
+
+
+# In[48]:
+
+
+a1[0]='your name'
+
+
+# In[49]:
+
+
+a1
+
+
+# In[50]:
+
+
+a1[0]="Dheeraj"
+
+
+# In[51]:
+
+
+a1
+
+
+# In[52]:
+
+
+len(a1)
+
+
+# In[53]:
+
+
+"kumar" in a1
+
+
+# In[54]:
+
+
+a2
+
+
+# In[55]:
+
+
+a2.append("third practice")
+
+
+# In[56]:
+
+
+a2
+
+
+# In[57]:
+
+
+a2.pop()
+
+
+# In[58]:
+
+
+a2
+
+
+# In[59]:
+
+
+a2.pop(1)
+
+
+# In[60]:
+
+
+a2
+
+
+# In[61]:
+
+
+a2.append("g")
+
+
+# In[62]:
+
+
+a2
+
+
+# In[63]:
+
+
+a2.insert(0,"k")
+
+
+# In[64]:
+
+
+a2
+
+
+# In[65]:
+
+
+a2.insert(2,[54,87,98,75])
+
+
+# In[66]:
+
+
+a2
+
+
+# In[67]:
+
+
+a2.count("xyz")
+
+
+# In[68]:
+
+
+a2.append(56)
+
+
+# In[69]:
+
+
+a2
+
+
+# In[70]:
+
+
+a2.pop()
+
+
+# In[71]:
+
+
+a2
+
+
+# In[ ]:
+
+
+a2.append
+
