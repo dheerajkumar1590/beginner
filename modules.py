8b8edc1c0d08534a4f7f772d0f16154bdda21164
dheerajkumar1590 <104754256+dheerajkumar1590@users.noreply.github.com>
2022-09-05 22:40:29 +0530

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+
+
+# In[ ]:
+
+
+def test(a,b):
+    return a+b
+
+
+# In[ ]:
+
+
+pwd()
+
+
+# In[ ]:
+
+
+dir()
+
+
+# In[ ]:
+
+
+ls
+
+
+# In[ ]:
+
+
+ls
+
+
+# In[ ]:
+
+
+import test
+
+
+# In[ ]:
+
+
+test.test(4,5)
+
+
+# In[1]:
+
+
+import test
+
+
+# In[2]:
+
+
+test.test4()
+
+
+# In[3]:
+
+
+test.test1(2,3)
+
+
+# In[4]:
+
+
+test.test2(2,3)
+
+
+# In[5]:
+
+
+from test import test4
+
+
+# In[6]:
+
+
+test4()
+
+
+# In[1]:
+
+
+import test
+
+
+# In[2]:
+
+
+test.test4()
+
+
+# In[1]:
+
+
+from test import *
+
+
+# In[2]:
+
+
+test1(4,5)
+
+
+# In[3]:
+
+
+test1(4,5)
+
+
+# In[5]:
+
+
+dir()
+
+
+# In[6]:
+
+
+open("mymodule.py","w")
+
+
+# In[7]:
+
+
+import mymodule
+
+
+# In[9]:
+
+
+mymodule.data
+
+
+# In[12]:
+
+
+mymodule.get_course()
+
+
+# In[13]:
+
+
+mymodule.greetings()
+
+
+# In[ ]:
+
+
+
+
